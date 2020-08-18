c1a10d441f67e4286ba3d0dd7dcee0a5e423e98e
Tristan Pacba <tpacba@live.com>
2020-08-18 01:35:41 -0700

    moved writing the md text to generatMarkdown.js, created require call for generateMarkdown

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs")
+const md = require("./utils/generateMarkdown")
 
 // array of questions for user
 const questions = [
@@ -60,19 +61,19 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-    let license = data.license.split(' ').join('');
-    data = `# ${data.title}\n\n` +
-    `![readmegenerator](https://img.shields.io/badge/license-${license}-red.svg)\n\n` +
-    `## Description\n\n${data.description}\n\n` +
-    `## Table of Contents\n\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contributing](#contributing)\n* [License](#license)\n* [Tests](#tests)\n* [Questions](#questions)\n\n` +
-    `## Installation\n\n${data.installation}\n\n` +
-    `## Usage\n\n${data.usage}\n\n` +
-    `## Contributing\n\n${data.contributing}\n\n` +
-    `## License\n\n${data.license}\n\n` +
-    `## Tests\n\n${data.tests}\n\n` +
-    `## Questions\n\nIf you have any questions, you can reach me through my email ${data.email} or connect with me on [GitHub](https://github.com/${data.github}).\n\n`;
+    // let license = data.license.split(' ').join('');
+    // data = `# ${data.title}\n\n` +
+    // `![readmegenerator](https://img.shields.io/badge/license-${license}-red.svg)\n\n` +
+    // `## Description\n\n${data.description}\n\n` +
+    // `## Table of Contents\n\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contributing](#contributing)\n* [License](#license)\n* [Tests](#tests)\n* [Questions](#questions)\n\n` +
+    // `## Installation\n\n${data.installation}\n\n` +
+    // `## Usage\n\n${data.usage}\n\n` +
+    // `## Contributing\n\n${data.contributing}\n\n` +
+    // `## License\n\n${data.license}\n\n` +
+    // `## Tests\n\n${data.tests}\n\n` +
+    // `## Questions\n\nIf you have any questions, you can reach me through my email ${data.email} or connect with me on [GitHub](https://github.com/${data.github}).\n\n`;
 
-    fs.writeFile(fileName, data, error => {
+    fs.writeFile(fileName, md(data), error => {
         if(error) {
             console.log(error)
         }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,18 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const license = data.license.split(' ').join('');
+  const text = `# ${data.title}\n\n` +
+    `![readmegenerator](https://img.shields.io/badge/license-${license}-red.svg)\n\n` +
+    `## Description\n\n${data.description}\n\n` +
+    `## Table of Contents\n\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contributing](#contributing)\n* [License](#license)\n* [Tests](#tests)\n* [Questions](#questions)\n\n` +
+    `## Installation\n\n${data.installation}\n\n` +
+    `## Usage\n\n${data.usage}\n\n` +
+    `## Contributing\n\n${data.contributing}\n\n` +
+    `## License\n\n${data.license}\n\n` +
+    `## Tests\n\n${data.tests}\n\n` +
+    `## Questions\n\nIf you have any questions, you can reach me through my email ${data.email} or connect with me on [GitHub](https://github.com/${data.github}).\n\n`;
 
-`;
+  return text;
 }
 
 module.exports = generateMarkdown;
