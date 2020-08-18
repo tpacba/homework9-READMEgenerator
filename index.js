9ecc33640f51aeaab6f1d8bd201ff6c876a4f2a9
Tristan Pacba <tpacba@live.com>
2020-08-18 02:09:07 -0700

    final push after testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs")
-const md = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // array of questions for user
 const questions = [
@@ -11,7 +11,7 @@ const questions = [
     },
     {
         type: "input",
-        message: "Description",
+        message: "Description:",
         name: "description"
     },
     {
@@ -61,19 +61,7 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-    // let license = data.license.split(' ').join('');
-    // data = `# ${data.title}\n\n` +
-    // `![readmegenerator](https://img.shields.io/badge/license-${license}-red.svg)\n\n` +
-    // `## Description\n\n${data.description}\n\n` +
-    // `## Table of Contents\n\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contributing](#contributing)\n* [License](#license)\n* [Tests](#tests)\n* [Questions](#questions)\n\n` +
-    // `## Installation\n\n${data.installation}\n\n` +
-    // `## Usage\n\n${data.usage}\n\n` +
-    // `## Contributing\n\n${data.contributing}\n\n` +
-    // `## License\n\n${data.license}\n\n` +
-    // `## Tests\n\n${data.tests}\n\n` +
-    // `## Questions\n\nIf you have any questions, you can reach me through my email ${data.email} or connect with me on [GitHub](https://github.com/${data.github}).\n\n`;
-
-    fs.writeFile(fileName, md(data), error => {
+    fs.writeFile(fileName, generateMarkdown(data), error => {
         if(error) {
             console.log(error)
         }
